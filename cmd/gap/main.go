de882828dcd4453a59ab850ae76bd913ad0fb615
g0dshadow1 <anastasleonov276@gmail.com>
2026-06-14 14:15:35 +0300

    feat: usage + exit 1

--- cmd/gap/main.go
+++ cmd/gap/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const version = "0.1"
 
 func main() {
 	if len(os.Args) < 2 {
-		return
+		usage()
+		os.Exit(1)
 	}
 
 	arg := os.Args[1]
@@ -17,8 +19,20 @@ func main() {
 	if arg == "hello" {
 		fmt.Println("hello, brother — это gap v" + version)
 		
+	}else if arg == "echo" {
+		if len(os.Args) < 3 {
+			usage()
+			os.Exit(1)
+		}
+		fmt.Println(strings.Join(os.Args[2:], " "))
 	}else{
-		fmt.Println("got:", arg)
+		usage()
+		os.Exit(1)
 	}
 
+}
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage:\n")
+	fmt.Fprintf(os.Stderr, " gap hello\n" )
+	fmt.Fprintf(os.Stderr, " gap echo <text>\n")
 }
