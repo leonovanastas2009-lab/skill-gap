fe240b1c1dd30a83997f0d535a7ba8d991d6d3c9
kayn <suvorovka04@gmail.com>
2026-06-16 01:34:04 +0300

    feat: add ci

--- cmd/gap/main.go
+++ cmd/gap/main.go
@@ -3,36 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 const version = "0.1"
 
 func main() {
 	if len(os.Args) < 2 {
-		usage()
-		os.Exit(1)
+		return
 	}
 
 	arg := os.Args[1]
 
 	if arg == "hello" {
 		fmt.Println("hello, brother — это gap v" + version)
-		
-	}else if arg == "echo" {
-		if len(os.Args) < 3 {
-			usage()
-			os.Exit(1)
-		}
-		fmt.Println(strings.Join(os.Args[2:], " "))
-	}else{
-		usage()
-		os.Exit(1)
-	}
 
-}
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage:\n")
-	fmt.Fprintf(os.Stderr, " gap hello\n" )
-	fmt.Fprintf(os.Stderr, " gap echo <text>\n")
+	} else {
+		fmt.Println("got:", arg)
+	}
 }
