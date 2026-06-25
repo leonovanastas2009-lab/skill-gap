ac8333c82f7d5a8342df35f6210b20eee9488e65
g0dshadow1 <anastasleonov276@gmail.com>
2026-06-25 22:42:23 +0300

    fix eror

--- cmd/gap/main.go
+++ cmd/gap/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"os"
 )
+
 const version = "0.1"
+
 func main() {
-	if len(os.Args) < 2{
+	if len(os.Args) < 2 {
 		usage()
 		os.Exit(1)
 	}
@@ -14,19 +16,17 @@ func main() {
 	arg := os.Args[1]
 
 	if arg == "hello" {
-		fmt.Println("hello, brother это gap v"+version)
+		fmt.Println("hello, brother это gap v" + version)
 	} else if arg == "echo" {
 		fmt.Println(os.Args[2:])
 	} else {
 		usage()
 		os.Exit(1)
 	}
-	
 
 }
 func usage() {
-		fmt.Fprintf(os.Stderr, "usage:\n")
-		fmt.Fprintf(os.Stderr, "gap hello\n")
-		fmt.Fprintf(os.Stderr, "gap echo ...\n")
-	}
-
+	fmt.Fprintf(os.Stderr, "usage:\n")
+	fmt.Fprintf(os.Stderr, "gap hello\n")
+	fmt.Fprintf(os.Stderr, "gap echo ...\n")
+}
