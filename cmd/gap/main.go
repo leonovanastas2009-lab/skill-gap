653dda7635a713044c14110630ae1c2faa1f4807
g0dshadow1 <anastasleonov276@gmail.com>
2026-06-25 22:36:04 +0300

    sam napisal code

--- cmd/gap/main.go
+++ cmd/gap/main.go
@@ -3,38 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
-
 const version = "0.1"
-
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 2{
 		usage()
 		os.Exit(1)
 	}
 
 	arg := os.Args[1]
 
-	switch arg {
-	case "hello":
-		fmt.Println("hello, brother — это gap v" + version)
-
-	case "echo":
-		if len(os.Args) < 3 {
-			usage()
-			os.Exit(1)
-		}
-
-		fmt.Println(strings.Join(os.Args[2:], " "))
-
-	default:
+	if arg == "hello" {
+		fmt.Println("hello, brother это gap v"+version)
+	} else if arg == "echo" {
+		fmt.Println(os.Args[2:])
+	} else {
 		usage()
 		os.Exit(1)
 	}
+	
+
 }
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage:\n")
-	fmt.Fprintf(os.Stderr, " gap hello\n")
-	fmt.Fprintf(os.Stderr, " gap echo <text>\n")
-}
+		fmt.Fprintf(os.Stderr, "usage:\n")
+		fmt.Fprintf(os.Stderr, "gap hello\n")
+		fmt.Fprintf(os.Stderr, "gap echo ...\n")
+	}
+
