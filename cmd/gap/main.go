8e2e11fe838d4dc15663d040688b2b250c7fcb44
g0dshadow1 <anastasleonov276@gmail.com>
2026-08-20 17:46:28 +0300

    feat: 3 команды - gap player / matches / match с валидацией

--- cmd/gap/main.go
+++ cmd/gap/main.go
@@ -15,6 +15,53 @@ func main() {
 	}
 
 	switch os.Args[1] {
+	case "match":
+		matchflags := flag.NewFlagSet("match", flag.ExitOnError)
+		id := matchflags.Int("id", 0, "id match")
+
+		if err := matchflags.Parse(os.Args[2:]); err != nil {
+			os.Exit(1)
+		}
+
+		if *id <= 0 {
+			fmt.Fprint(os.Stderr, "invalid id")
+			os.Exit(1)
+
+		} else {
+			fmt.Fprintf(os.Stdout, "ID match = %d\n", *id)
+		}
+
+	case "matches":
+		matchesflags := flag.NewFlagSet("matches", flag.ExitOnError)
+		last := matchesflags.Int("last", 10, "last matches")
+
+		if err := matchesflags.Parse(os.Args[2:]); err != nil {
+			os.Exit(1)
+		}
+
+		if *last > 50 {
+			fmt.Fprint(os.Stderr, "Limit is 50")
+			os.Exit(1)
+		} else {
+			fmt.Fprintf(os.Stdout, "matches для 12345678, last= %d (stub)", *last)
+		}
+
+	case "player":
+		playerflags := flag.NewFlagSet("player", flag.ExitOnError)
+		id := playerflags.Int("id", 0, "id accaunt")
+
+		if err := playerflags.Parse(os.Args[2:]); err != nil {
+			os.Exit(1)
+		}
+
+		if *id <= 0 {
+			fmt.Fprint(os.Stderr, "invalid id")
+			os.Exit(1)
+
+		} else {
+			fmt.Fprintf(os.Stdout, "ID Игока = %d\n", *id)
+		}
+
 	case "hello":
 		helloflags := flag.NewFlagSet("hello", flag.ExitOnError)
 		name := helloflags.String("name", "brother", "имя")
@@ -35,11 +82,14 @@ func main() {
 	default:
 		usage()
 		os.Exit(1)
-	}
 
+	}
 }
 
 func usage() {
+	fmt.Fprintln(os.Stderr, "match --id=...")
+	fmt.Fprintln(os.Stderr, "matches --last=...")
+	fmt.Fprintln(os.Stderr, "player --id=...")
 	fmt.Fprintln(os.Stderr, "usage:")
 	fmt.Fprintln(os.Stderr, "gap hello")
 	fmt.Fprintln(os.Stderr, "gap echo ...")
